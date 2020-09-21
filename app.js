075c7d4b51d90e9b9ed9fa0a94daf92de9105426
ibrarsoomro <50750853+ibrarsoomro@users.noreply.github.com>
2020-09-22 00:35:07 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Arrow Function
+
+ let hello = (name) => {
+     console.log(this)
+ }
+
+console.log(hello(name))
+
+hello( )
+
+
+// functtion expression
+// let too = function  () {
+//     console.log("hellow world")
+// }
+
+// too()
+
+
+ // destructring of Array
+//  let name = ["ibrar", "Soomro", "ahmed"]
+
+//  let [name1 , name2 , name3] = name;
+
+//  console.log(name3);
+
+
+
+ // Object Destructring
+// let foo = {
+//     name : "ibrar",
+//     email : "ibrar@gmail.co,"
+// }
+
+// let {name, email} = foo;
+
+
+// console.log(name,email)
